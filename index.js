1d0cd9f3ed085ee3e6a9eaf82f6492f6fc913653
Le0sy <leo.sooyeonlee@gmail.com>
2021-06-28 00:30:01 +0900

    #13 Auth

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const { User } = require('./models/User');
 
 const config = require('./config/key');
 
+const { auth } = require('./middleware/auth')
+
 
 
 const mongoose = require('mongoose')
@@ -26,7 +28,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World! 테스트 입니다.'))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     // 회원 가입 시 필요한 정보를 Client에서 가져오면,
     // 해당 정보를 DB에 넣어줌.
     const user = new User(req.body)
@@ -40,7 +42,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     // 요청된 이메일을 DB에서 탐색
     User.findOne( { email: req.body.email }, (err, user) => {
         if(!user) { // user 데이터가 넘어오지 않았다면
@@ -65,5 +67,21 @@ app.post('/login', (req, res) => {
         })
     })
 })
+                           // end point의 리퀘스트를 받고, callback 함수 이전에 특정 동작 수행
+app.get('/api/users/auth', auth , (req, res) => {
+    // 아래를 수행한다는 것은 auth가 true임.
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening at ${port}!`))
